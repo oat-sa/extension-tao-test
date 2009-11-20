f2c0e996b282822a0af41434d7baf68f7c70ef71
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2009-11-20 16:12:08 +0000

    integrate common structure
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@1646 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/Tests.class.php
+++ actions/Tests.class.php
@@ -1,17 +1,66 @@
 <?php
-class Tests extends Module {
+require_once('tao/actions/CommonModule.class.php');
+require_once('tao/actions/TaoModule.class.php');
 
-	protected $service = null;
+/**
+ * Tests Controller provide actions performed from url resolution
+ * 
+ * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @package taoTests
+ * @subpackage actions
+ */
+class Tests extends TaoModule {
+
 
 	public function __construct(){
+	
+		parent::__construct();
+	
+		//the service is initialized by default
 		$this->service = tao_models_classes_ServiceFactory::get('Tests');
+		$this->defaultData();
 	}
+	
+/*
+ * conveniance methods
+ */
+	
+	/**
+	 * get the instancee of the current test regarding the 'uri' and 'classUri' request parameters
+	 * @return core_kernel_classes_Resource the test instance
+	 */
+	private function getCurrentTest(){
+		
+		$uri = tao_helpers_Uri::decode($this->getRequestParameter('uri'));
+		if(is_null($uri) || empty($uri)){
+			throw new Exception("No valid uri found");
+		}
+		
+		$clazz = $this->getCurrentClass();
+		
+		$test = $this->service->getTest($uri, 'uri', $clazz);
+		if(is_null($test)){
+			throw new Exception("No test found for the uri {$uri}");
+		}
+		
+		return $test;
+	}
+	
+/*
+ * controller actions
+ */
 	
 	/**
 	 * main action
 	 * @return void
 	 */
 	public function index(){
+		
+		if($this->getData('reload') == true){
+			unset($_SESSION[SESSION_NAMESPACE]['uri']);
+			unset($_SESSION[SESSION_NAMESPACE]['classUri']);
+		}
+		
 		$context = Context::getInstance();
 		$this->setData('content', "this is the ". get_class($this) ." module, " . $context->getActionName());
 		$this->setView('index.tpl');
@@ -34,5 +83,155 @@ class Tests extends Module {
 		} 
 		echo json_encode( $this->service->toTree( $this->service->getTestClass(), true, true, $highlightUri));
 	}
+	
+	/**
+	 * Add an test instance
+	 * @return void
+	 */
+	public function addTest(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		$clazz = $this->getCurrentClass();
+		$test = $this->service->createInstance($clazz);
+		if(!is_null($test) && $test instanceof core_kernel_classes_Resource){
+			echo json_encode(array(
+				'label'	=> $test->getLabel(),
+				'uri' 	=> tao_helpers_Uri::encode($test->uriResource)
+			));
+		}
+	}
+	
+	/**
+	 * edit a test instance
+	 */
+	public function editTest(){
+		$clazz = $this->getCurrentClass();
+		$test = $this->getCurrentTest();
+		$myForm = tao_helpers_form_GenerisFormFactory::instanceEditor($clazz, $test);
+		if($myForm->isSubmited()){
+			if($myForm->isValid()){
+				
+				$test = $this->service->bindProperties($test, $myForm->getValues());
+				
+				$this->setSessionAttribute("showNodeUri", tao_helpers_Uri::encode($test->uriResource));
+				$this->setData('message', 'Test saved');
+				$this->setData('reload', true);
+				$this->forward('Tests', 'index');
+			}
+		}
+		
+		$this->setData('formTitle', 'Edit test');
+		$this->setData('myForm', $myForm->render());
+		$this->setView('form.tpl');
+	}
+	
+	/**
+	 * add a test (subclass Test)
+	 * @return void
+	 */
+	public function addTestClass(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		$clazz = $this->service->createTestClass($this->getCurrentClass());
+		if(!is_null($clazz) && $clazz instanceof core_kernel_classes_Class){
+			echo json_encode(array(
+				'label'	=> $clazz->getLabel(),
+				'uri' 	=> tao_helpers_Uri::encode($clazz->uriResource)
+			));
+		}
+	}
+	
+	/**
+	 * Edit a test model (edit a class)
+	 * @return void
+	 */
+	public function editTestClass(){
+		$myForm = $this->editClass($this->getCurrentClass(), $this->service->getTestClass());
+		if($myForm->isSubmited()){
+			if($myForm->isValid()){
+				if($clazz instanceof core_kernel_classes_Resource){
+					$this->setSessionAttribute("showNodeUri", tao_helpers_Uri::encode($clazz->uriResource));
+				}
+				$this->setData('message', 'class saved');
+				$this->setData('reload', true);
+				$this->forward('Tests', 'index');
+			}
+		}
+		$this->setData('formTitle', 'Edit test class');
+		$this->setData('myForm', $myForm->render());
+		$this->setView('form.tpl');
+	}
+	
+	/**
+	 * delete a test or a test class
+	 * called via ajax
+	 * @return void
+	 */
+	public function delete(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		
+		$deleted = false;
+		if($this->getRequestParameter('uri')){
+			$deleted = $this->service->deleteTest($this->getCurrentTest());
+		}
+		else{
+			$deleted = $this->service->deleteTestClass($this->getCurrentClass());
+		}
+		
+		echo json_encode(array('deleted'	=> $deleted));
+	}
+	
+	/**
+	 * duplicate a test instance by property copy
+	 * @return void
+	 */
+	public function cloneTest(){
+		if(!tao_helpers_Request::isAjax()){
+			throw new Exception("wrong request mode");
+		}
+		
+		$test = $this->getCurrentTest();
+		$clazz = $this->getCurrentClass();
+		
+		$clone = $this->service->createInstance($clazz);
+		if(!is_null($clone)){
+			
+			foreach($clazz->getProperties() as $property){
+				foreach($test->getPropertyValues($property) as $propertyValue){
+					$clone->setPropertyValue($property, $propertyValue);
+				}
+			}
+			$clone->setLabel($test->getLabel()."'");
+			echo json_encode(array(
+				'label'	=> $clone->getLabel(),
+				'uri' 	=> tao_helpers_Uri::encode($clone->uriResource)
+			));
+		}
+	}
+	
+	/*
+	 * @TODO implement the following actions
+	 */
+	
+	public function getMetaData(){
+		throw new Exception("Not yet implemented");
+	}
+	
+	public function saveComment(){
+		throw new Exception("Not yet implemented");
+	}
+	
+	public function import(){
+		throw new Exception("Not yet implemented");
+	}
+	
+	public function export(){
+		throw new Exception("Not yet implemented");
+	}
+
 }
 ?>
--- models/classes/class.TestsService.php
+++ models/classes/class.TestsService.php
@@ -9,8 +9,8 @@ error_reporting(E_ALL);
  *
  * This file is part of Generis Object Oriented API.
  *
- * Automatically generated on 30.10.2009, 09:53:58 with ArgoUML PHP module 
- * (last revised $Date: 2008-04-19 08:22:08 +0200 (Sat, 19 Apr 2008) $)
+ * Automatically generated on 20.11.2009, 17:08:29 with ArgoUML PHP module 
+ * (last revised $Date: 2009-04-11 21:57:46 +0200 (Sat, 11 Apr 2009) $)
  *
  * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
  * @package taoTests
@@ -93,6 +93,7 @@ class taoTests_models_classes_TestsService
         // section 10-13-1-45-2836570e:123bd13e69b:-8000:0000000000001888 begin
 		
 		parent::__construct();
+		
 		$this->testClass = new core_kernel_classes_Class(TEST_CLASS);
 		$this->loadOntologies($this->testsOntologies);
 		
@@ -125,13 +126,23 @@ class taoTests_models_classes_TestsService
      * @access public
      * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
      * @param  string identifier usually the test label or the ressource URI
+     * @param  string mode
+     * @param  Class clazz
      * @return core_kernel_classes_Resource
      */
-    public function getTest($identifier)
+    public function getTest($identifier, $mode = 'uri',  core_kernel_classes_Class $clazz = null)
     {
         $returnValue = null;
 
         // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017D8 begin
+		
+		if(is_null($clazz)){
+			$clazz = $this->testClass;
+		}
+		if($this->isTestClass($clazz)){
+			$returnValue = $this->getOneInstanceBy( $clazz, $identifier, $mode);
+		}
+		
         // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017D8 end
 
         return $returnValue;
@@ -188,6 +199,11 @@ class taoTests_models_classes_TestsService
         $returnValue = (bool) false;
 
         // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017E6 begin
+		
+		if(!is_null($test)){
+			$returnValue = $test->delete();
+		}
+		
         // section 10-13-1-45-792423e0:12398d13f24:-8000:00000000000017E6 end
 
         return (bool) $returnValue;
@@ -207,11 +223,115 @@ class taoTests_models_classes_TestsService
 
         // section 127-0-1-1-5109b15:124a4877945:-8000:0000000000001AF3 begin
 		
-		$returnValue = $this->testClass;
+		if(empty($uri) && !is_null($this->testClass)){
+			$returnValue = $this->testClass;
+		}
+		else{
+			$clazz = new core_kernel_classes_Class($uri);
+			if($this->isTestClass($clazz)){
+				$returnValue = $clazz;
+			}
+		}
 		
         // section 127-0-1-1-5109b15:124a4877945:-8000:0000000000001AF3 end
 
         return $returnValue;
+    }
+
+    /**
+     * Short description of method createTestClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Class clazz
+     * @param  string label
+     * @param  array properties
+     * @return core_kernel_classes_Class
+     */
+    public function createTestClass( core_kernel_classes_Class $clazz = null, $label = '', $properties = array())
+    {
+        $returnValue = null;
+
+        // section 127-0-1-1--728644f3:12512379b22:-8000:0000000000001C2B begin
+		
+		if(is_null($clazz)){
+			$clazz = $this->testClass;
+		}
+		
+		if($this->isTestClass($clazz)){
+		
+			$testClass = $this->createSubClass($clazz, $label);
+			
+			foreach($properties as $propertyName => $propertyValue){
+				$myProperty = $subjectClass->createProperty(
+					$propertyName,
+					$propertyName . ' ' . $label .' test property created from ' . get_class($this) . ' the '. date('Y-m-d h:i:s') 
+				);
+				
+				//@todo implement check if there is a widget key and/or a range key
+			}
+			$returnValue = $testClass;
+		}
+		
+        // section 127-0-1-1--728644f3:12512379b22:-8000:0000000000001C2B end
+
+        return $returnValue;
+    }
+
+    /**
+     * Short description of method isTestClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Class clazz
+     * @return boolean
+     */
+    public function isTestClass( core_kernel_classes_Class $clazz)
+    {
+        $returnValue = (bool) false;
+
+        // section 127-0-1-1--728644f3:12512379b22:-8000:0000000000001C3C begin
+		
+		if($clazz->uriResource == $this->testClass->uriResource){
+			$returnValue = true;	
+		}
+		else{
+			foreach($this->testClass->getSubClasses(true) as $subclass){
+				if($clazz->uriResource == $subclass->uriResource){
+					$returnValue = true;
+					break;	
+				}
+			}
+		}
+		
+        // section 127-0-1-1--728644f3:12512379b22:-8000:0000000000001C3C end
+
+        return (bool) $returnValue;
+    }
+
+    /**
+     * Short description of method deleteTestClass
+     *
+     * @access public
+     * @author Bertrand Chevrier, <chevrier.bertrand@gmail.com>
+     * @param  Class clazz
+     * @return boolean
+     */
+    public function deleteTestClass( core_kernel_classes_Class $clazz)
+    {
+        $returnValue = (bool) false;
+
+        // section 127-0-1-1--728644f3:12512379b22:-8000:0000000000001C40 begin
+		
+		if(!is_null($clazz)){
+			if($this->isTestClass($clazz) && $clazz->uriResource != $this->testClass->uriResource){
+				$returnValue = $clazz->delete();
+			}
+		}
+		
+        // section 127-0-1-1--728644f3:12512379b22:-8000:0000000000001C40 end
+
+        return (bool) $returnValue;
     }
 
 } /* end of class taoTests_models_classes_TestsService */
