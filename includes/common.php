a72e77dccb12c81a884245f13fa961117adeeaeb
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-05-17 13:32:48 +0000

    * QCM: fix bug #0000276 on "add inqueiries and propositions"  * some minor UI fixs
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@2664 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/common.php
+++ includes/common.php
@@ -7,4 +7,5 @@ require_once dirname(__FILE__). '/constants.php';
 set_include_path(get_include_path() . PATH_SEPARATOR . GENERIS_BASE_PATH.'/..');
 
 include_once 'tao/includes/prepend.php';
+require_once 'tao/helpers/class.Uri.php';
 ?>
