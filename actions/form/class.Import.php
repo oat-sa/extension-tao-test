2f9c5b5f4455b6bf67a4ff16ddad5b203263a832
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-10-15 14:36:36 +0000

    TAO Items:  * new Import form: enable to import QTI items
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@3444 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ actions/form/class.Import.php
@@ -0,0 +1,89 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * TAO - taoTests/actions/form/class.Import.php
+ *
+ * $Id$
+ *
+ * This file is part of TAO.
+ *
+ * Automatically generated on 15.10.2010, 16:27:33 with ArgoUML PHP module 
+ * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
+ *
+ * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+ * @package taoTests
+ * @subpackage actions_form
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/**
+ * include
+ *
+ * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+ */
+require_once('class..php');
+
+/**
+ * This container initialize the import form.
+ *
+ * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+ */
+require_once('tao/actions/form/class.Import.php');
+
+/* user defined includes */
+// section 127-0-1-1-2993bc96:12baebd89c3:-8000:0000000000002659-includes begin
+// section 127-0-1-1-2993bc96:12baebd89c3:-8000:0000000000002659-includes end
+
+/* user defined constants */
+// section 127-0-1-1-2993bc96:12baebd89c3:-8000:0000000000002659-constants begin
+// section 127-0-1-1-2993bc96:12baebd89c3:-8000:0000000000002659-constants end
+
+/**
+ * Short description of class taoTests_actions_form_Import
+ *
+ * @access public
+ * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+ * @package taoTests
+ * @subpackage actions_form
+ */
+class taoTests_actions_form_Import
+    /* multiple generalisations not supported by PHP: */
+    /* extends ,
+            tao_actions_form_Import */
+{
+    // --- ASSOCIATIONS ---
+
+
+    // --- ATTRIBUTES ---
+
+    /**
+     * Short description of attribute formats
+     *
+     * @access protected
+     * @var array
+     */
+    protected $formats = array('csv', 'qti');
+
+    // --- OPERATIONS ---
+
+    /**
+     * Short description of method initQTIElements
+     *
+     * @access public
+     * @author Bertrand Chevrier, <bertrand.chevrier@tudor.lu>
+     * @return mixed
+     */
+    public function initQTIElements()
+    {
+        // section 127-0-1-1--67198282:12bb0429ae8:-8000:0000000000002688 begin
+        // section 127-0-1-1--67198282:12bb0429ae8:-8000:0000000000002688 end
+    }
+
+} /* end of class taoTests_actions_form_Import */
+
+?>
