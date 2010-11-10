a43c6f981ae11ea3c71fdd5e4968197e051815f8
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2010-11-10 09:51:34 +0000

    test model updated with authoring mode management
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@3613 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -5,11 +5,10 @@
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  *
  */
-
-include_once ROOT_PATH . '/tao/includes/constants.php';
-
 //include constants for the wfEngine:
 include_once ROOT_PATH . '/wfEngine/includes/constants.php';
+
+include_once ROOT_PATH . '/tao/includes/constants.php';
 
 $todefine = array(
 	'TEST_RELATED_ITEMS_PROP' 			=> 'http://www.tao.lu/Ontologies/TAOTest.rdf#RelatedItems',
@@ -18,9 +17,9 @@ $todefine = array(
 	'TAO_ITEM_MODEL_RUNTIME_PROPERTY' 	=> 'http://www.tao.lu/Ontologies/TAOItem.rdf#SWFFile',
 	'TEST_CONTENT_REF_FILE'				=> BASE_PATH.'/data/test_content_ref.xml',
 	'TEST_TESTPROCESS'				=> 'http://www.tao.lu/Ontologies/TAOItem.rdf#TestProcess',
-	'TAO_TEST_AUTHORINGMODE_PROP' => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#AuthoringMode',
-	'TAO_TEST_SIMPLEMODE' => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#i1268049036038811802',
-	'TAO_TEST_ADVANCEDMODE' => 'http://www.tao.lu/Ontologies/TAODelivery.rdf#i1268049036038811803'
+	'TAO_TEST_AUTHORINGMODE_PROP' => 'http://www.tao.lu/Ontologies/TAOTest.rdf#AuthoringMode',
+	'TAO_TEST_SIMPLEMODE' => 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811802',
+	'TAO_TEST_ADVANCEDMODE' => 'http://www.tao.lu/Ontologies/TAOTest.rdf#i1268049036038811803'
 );
 foreach($todefine as $constName => $constValue){
 	if(!defined($constName)){
