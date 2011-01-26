bd7d72f85ce96ba9ea9e25fb7da784f7bdb21d47
bchevrier <bchevrier@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-01-26 14:37:45 +0000

    * Update extensions manifests to include model dependancy
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@4352 62dfeb86-a93f-4914-9d13-15c0a160985d

--- manifest.php
+++ manifest.php
@@ -1,35 +1,25 @@
 <?php
-
 /*
  * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  * 
  */
-	return array(
-		'name' => 'TAO Tests',
-		'description' => 'TAO Tests extensions http://www.tao.lu',
-		'additional' => array(
-			'version' => '1.2',
-			'author' => 'CRP Henri Tudor',
-			'dependances' => array(),
-			'install' => array( 
-				'sql' => dirname(__FILE__). '/model/ontology/TAOTest.sql',
-				'php' => dirname(__FILE__). '/install/install.php'
-			),
-			'model' => array('http://www.tao.lu/Ontologies/TAOTest.rdf', 
-							'http://www.tao.lu/Ontologies/TAOItem.rdf'
-			),
-		
-
-			'classLoaderPackages' => array( 
-				dirname(__FILE__).'/actions/',
-				dirname(__FILE__).'/helpers/'
-			 )
-
-			
-
-				
-			
-		)
-	);
+return array(
+	'name' => 'taoTests',
+	'description' => 'TAO Tests extension',
+	'additional' => array(
+		'version' => '2.0',
+		'author' => 'CRP Henri Tudor',
+		'dependances' => array('wfEngine'),
+		'models' => 'http://www.tao.lu/Ontologies/TAOTest.rdf',
+		'install' => array( 
+			'php' => dirname(__FILE__). '/install/install.php',
+			'rdf' => dirname(__FILE__). '/models/ontology/taotest.rdf'
+		),
+		'classLoaderPackages' => array( 
+			dirname(__FILE__).'/actions/',
+			dirname(__FILE__).'/helpers/'
+		 )
+	)
+);
 ?>
