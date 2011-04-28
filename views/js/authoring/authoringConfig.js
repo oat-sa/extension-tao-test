fd7d045a96d5d2b319ba1151341cba61934d76dc
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-04-28 11:35:55 +0000

    make process authoring work in an installation of TAO in a subfolder of document root
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@5551 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/authoring/authoringConfig.js
+++ views/js/authoring/authoringConfig.js
@@ -1,3 +1,3 @@
-var authoringControllerPath = '/taoTests/TestAuthoring/';
+var authoringControllerPath = root_url + '/taoTests/TestAuthoring/';
 var img_url = root_url + "/taoDelivery/views/img/";//change to "/taoTests/views/img/" after image copy and selection is done
 
