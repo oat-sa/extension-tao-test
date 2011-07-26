40b2f736dbac43ccdc1d0a1863135a812f6de449
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2011-07-26 11:59:55 +0000

    PO Update
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@6504 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ locales/_raw/messages_po.js
@@ -0,0 +1,4 @@
+/* auto generated content */
+/* extesion: taoTests, lang: _raw */
+var langCode = '_raw';
+var i18n_tr = {"Advanced Authoring Tool":"","Class saved":"","Drag and drop the items to order them":"","Edit test class":"","Items sequence":"","Manage tests":"","Save":"","Select related items":"","Sequence saved successfully":"","Test properties":"","Test saved":"","Tests":"","Tests library":"","The Test mode (sequencing, scoring, cumulating, etc.) and layout are also configured here.":"","The Test module allows to combine a selection of Items to a Test.":"","advanced mode":"","create table":"","delete":"","duplicate":"","export":"","import":"","lists":"","meta data":"","move":"","new class":"","new test":"","search":"","simple mode":""};
