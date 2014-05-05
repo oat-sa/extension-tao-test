720c6724c4a3c891f54986c58541f41a6d65ce78
pplichart <pplichart@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-05 15:07:36 +0000

    changed description (ran taoTranslate.php -v -a compile -l en-US -e taoTests and update )
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoTests/trunk@20058 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -3,6 +3,6 @@
 define(function(){
  return {
    langCode : 'en-US',
-   i18n_tr : []
+   i18n_tr : {"The Test mode (sequencing, scoring, cumulating, etc.) and layout are also configured here.":"Configure the test mode (sequencing, scoring, etc.) and layout.","The Tests module allows to combine a selection of Items to a Test.":"Choose which items you want to include in a test.\u0000"}
  };
 });
