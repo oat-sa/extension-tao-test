e4f5a15c403323e4a9fe69f46c69eb86f41a3819
Lionel Lecaque <lionel@taotesting.com>
2014-10-14 13:30:50 +0200

    update locales

--- locales/en-US/messages_po.js
+++ locales/en-US/messages_po.js
@@ -1,1 +1,1 @@
-{"Group test takers according to global features and classifications.":"Group test-takers according to global features and classifications.","Manage test takers":"Manage test-takers","Record and manage test takers.":"Record and manage test-takers.","The Test mode (sequencing, scoring, cumulating, etc.) and layout are also configured here.":"Configure the test mode (sequencing, scoring, etc.) and layout.","The Tests module allows to combine a selection of Items to a Test.":"Choose which items you want to include in a test.\u0000"}
+[]
