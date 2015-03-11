98960190f56153dc75395ec719c5f72e4dd2c0d9
Joel Bout <joel@taotesting.com>
2015-03-11 10:40:17 +0100

    use lock register()

--- views/js/controller/tests/editTest.js
+++ views/js/controller/tests/editTest.js
@@ -1,26 +1,10 @@
 
-define(['module', 'jquery','helpers','ui/lock', 'ui/feedback', 'i18n'],
-	function(module, $, helpers, lock, feedback, __){
+define(['module', 'jquery','ui/lock'],
+	function(module, $, lock){
 
         var editTestController = {
             start : function(options){
-                var config = module.config();
-        
-            	var $lockDiv = $('#lock-box');
-                if($lockDiv.length == 1){
-                    var lk = lock($lockDiv).hasLock($lockDiv.data('msg'),
-                        {
-                            released : function() {
-                            	feedback().success(__('The test has been released'));
-                                this.close();
-                            },
-                            failed : function() {
-                            	feedback().error(__('The test could not be released'));
-                            },
-                            url: helpers._url('release','Lock','tao'),
-                            uri: $lockDiv.data('id')
-                        });
-                }
+            	$('#lock-box').each(function() {lock($(this)).register()});
             }
         };
 
