c7e545e8b36732bc1d9ee32ab956f763ba05293c
Ivan Klimchuk <klimchuk@1pt.com>
2015-08-28 16:44:32 +0300

    Added check locks and remove it when user want remove own test on other tabs

--- actions/class.Tests.php
+++ actions/class.Tests.php
@@ -121,19 +121,30 @@ class taoTests_actions_Tests extends tao_actions_SaSModule {
      */
     public function delete()
 	{
-		if(!tao_helpers_Request::isAjax()){
+		if (!tao_helpers_Request::isAjax()) {
 			throw new Exception("wrong request mode");
 		}
 
 		$deleted = false;
-		if($this->getRequestParameter('uri')){
-			$deleted = $this->service->deleteTest($this->getCurrentInstance());
-		}
-		else{
+		if ($this->getRequestParameter('uri')) {
+
+			$instance = $this->getCurrentInstance();
+
+			$userService = tao_models_classes_UserService::singleton();
+			$user = $userService->getCurrentUser();
+
+			$lockManager = LockManager::getImplementation();
+
+			if ($lockManager->isLocked($instance)) {
+				$lockManager->releaseLock($instance, $user->getUri());
+			}
+
+			$deleted = $this->service->deleteTest($instance);
+		} else {
 			return $this->forward('deleteClass', null, null, (array('id' => $this->getRequestParameter('id'))));
 		}
 
-		echo json_encode(array('deleted'	=> $deleted));
+		echo json_encode(array('deleted' => $deleted));
 	}
 
 
