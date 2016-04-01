bc246823557de82b8c94d4d9f6c05e13e6ca4480
jsconan <jsconan@gmail.com>
2016-04-01 16:30:29 +0200

    Make TimePoint a class instead of an interface

--- models/classes/runner/time/TimePoint.php
+++ models/classes/runner/time/TimePoint.php
@@ -26,7 +26,7 @@ namespace oat\taoTests\models\runner\time;
  * Interface TimePoint
  * @package oat\taoTests\models\runner\time
  */
-interface TimePoint extends \Serializable
+class TimePoint implements \Serializable
 {
     /**
      * Type of TimePoint: start of range
@@ -59,76 +59,260 @@ interface TimePoint extends \Serializable
     const TARGET_ALL = 3;
 
     /**
+     * The timestamp representing the TimePoint
+     * @var float
+     */
+    protected $timestamp = 0.0;
+
+    /**
+     * A collection of tags attached to the TimePoint
+     * @var array
+     */
+    protected $tags = [];
+
+    /**
+     * The type of TimePoint. Must be a value from TYPE_START or TYPE_END constants.
+     * @var int
+     */
+    protected $type = 0;
+
+    /**
+     * The type of target. Must be a value from TARGET_CLIENT or TARGET_SERVER constants.
+     * @var int
+     */
+    protected $target = 0;
+
+    /**
+     * QtiTimePoint constructor.
+     * @param string|array $tags
+     * @param float $timestamp
+     * @param int $type
+     * @param int $target
+     */
+    public function __construct($tags = null, $timestamp = null, $type = null, $target = null)
+    {
+        if (isset($tags)) {
+            $this->setTags($tags);
+        }
+
+        if (isset($timestamp)) {
+            $this->setTimestamp($timestamp);
+        }
+
+        if (isset($type)) {
+            $this->setType($type);
+        }
+
+        if (isset($target)) {
+            $this->setTarget($target);
+        }
+    }
+
+    /**
+     * String representation of object
+     * @link http://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        $data = [
+            'ts' => $this->getTimestamp(),
+            'type' => $this->getType(),
+            'target' => $this->getTarget(),
+            'tags' => $this->getTags(),
+        ];
+        return serialize($data);
+    }
+
+    /**
+     * Constructs the object
+     * @link http://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($serialized)
+    {
+        $data = unserialize($serialized);
+
+        if ($data) {
+            if (isset($data['tags'])) {
+                $this->setTags($data['tags']);
+            }
+
+            if (isset($data['ts'])) {
+                $this->setTimestamp($data['ts']);
+            }
+
+            if (isset($data['type'])) {
+                $this->setType($data['type']);
+            }
+
+            if (isset($data['target'])) {
+                $this->setTarget($data['target']);
+            }
+        }
+    }
+
+    /**
      * Sets the timestamp of the TimePoint
      * @param float $timestamp
      * @return TimePoint
      */
-    public function setTimestamp($timestamp);
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = floatval($timestamp);
+        return $this;
+    }
 
     /**
      * Gets the timestamp of the TimePoint
      * @return float
      */
-    public function getTimestamp();
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
 
     /**
      * Sets the type of TimePoint
      * @param int $type Must be a value from TYPE_START or TYPE_END constants.
      * @return TimePoint
      */
-    public function setType($type);
+    public function setType($type)
+    {
+        $this->type = intval($type);
+        return $this;
+    }
 
     /**
      * Gets the type of TimePoint
      * @return int
      */
-    public function getType();
+    public function getType()
+    {
+        return $this->type;
+    }
 
     /**
      * Sets the target type of the TimePoint
      * @param int $target Must be a value from TARGET_CLIENT or TARGET_SERVER constants.
      * @return TimePoint
      */
-    public function setTarget($target);
+    public function setTarget($target)
+    {
+        $this->target = intval($target);
+        return $this;
+    }
 
     /**
      * Gets the target type of the TimePoint
      * @return int
      */
-    public function getTarget();
+    public function getTarget()
+    {
+        return $this->target;
+    }
 
     /**
      * Adds another tag to the TimePoint
      * @param string $tag
      * @return TimePoint
      */
-    public function addTag($tag);
+    public function addTag($tag)
+    {
+        $this->tags []= (string)$tag;
+        return $this;
+    }
 
     /**
      * Remove a tag from the TimePoint
      * @param string $tag
      * @return TimePoint
      */
-    public function removeTag($tag);
+    public function removeTag($tag)
+    {
+        $index = array_search($tag, $this->tags);
+
+        if ($index !== false) {
+            array_splice($this->tags, $index, 1);
+        }
+
+        return $this;
+    }
 
     /**
      * Gets a tag from the TimePoint. By default, it will return the first tag.
      * @param int $index
      * @return string
      */
-    public function getTag($index = 0);
+    public function getTag($index = 0)
+    {
+        $index = min(max(0, $index), count($this->tags));
+        return $this->tags[$index];
+    }
 
     /**
      * Sets the tags of the TimePoint
-     * @param array $tags
+     * @param string|array $tags
      * @return TimePoint
      */
-    public function setTags($tags);
-    
+    public function setTags($tags)
+    {
+        $this->tags = [];
+
+        if (is_array($tags)) {
+            foreach($tags as $tag) {
+                $this->addTag($tag);
+            }
+        } else {
+            $this->addTag($tags);
+        }
+
+        return $this;
+    }
+
     /**
      * Gets all tags from the TimePoint
      * @return array
      */
-    public function getTags();
+    public function getTags()
+    {
+        return $this->tags;
+    }
 
+    /**
+     * Checks if a TimePoint matches the criteria
+     * @param array $tags
+     * @param int $target
+     * @param int $type
+     * @return array
+     */
+    public function match(array $tags, $target = self::TARGET_ALL, $type = self::TYPE_ALL)
+    {
+        return ($this->getType() & $type) && ($this->getTarget() & $target) && count(array_intersect($tags, $this->getTags()));
+    }
+
+    /**
+     * Compare the TimePoint with another instance
+     * @param TimePoint $point
+     * @return int
+     */
+    public function compare(TimePoint $point)
+    {
+        $diff = $point->getTimestamp() - $this->getTimestamp();
+        if ($diff == 0) {
+            $diff = $point->getType() - $this->getType();
+            if (!$diff) {
+                $diff = $point->getTarget() - $this->getTarget();
+                if (!$diff) {
+                    $diff = count(array_diff($point->getTags(), $this->getTags()));
+                }
+            }
+        }
+        return intval($diff);
+    }
 }
