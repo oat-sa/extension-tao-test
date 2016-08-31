c6dffe5c40badf13009f4b58142e4f525e05e6ba
antoinerobin <antoine.robin@vesperiagroup.com>
2016-08-31 15:14:13 +0200

    add unit test on get test model implementation

--- test/TestsTest.php
+++ test/TestsTest.php
@@ -19,10 +19,11 @@
  *               2014 (update and modification) Open Assessment Technologies SA
  *               
  */
-namespace oat\taoTestTaker\test;
+namespace oat\taoTests\test;
 
+use oat;
 use oat\tao\test\TaoPhpUnitTestRunner;
-use \tao_models_classes_Service;
+use Prophecy\Prediction\CallTimesPrediction;
 use \taoTests_models_classes_TestsService;
 use \core_kernel_classes_Class;
 use \core_kernel_classes_Resource;
@@ -108,6 +109,65 @@ class TestsTestCase extends TaoPhpUnitTestRunner {
             $this->testsService->setTestModel($test, $model);        
             $this->assertEquals($this->testsService->getTestModel($test)->getUri(),$uri);
         }
+    }
+    
+    
+    /**
+     * @expectedException \common_exception_Error
+     * @expectedExceptionMessage Test model service FakeTestModelClass not found
+     */
+    public function testGetTestModelImplementationBackwardCompatibleFakeClass(){
+        $testModelProphecy = $this->prophesize('\core_kernel_classes_Resource');
+        $testModelProphecy->getOnePropertyValue(new core_kernel_classes_Property(PROPERTY_TESTMODEL_IMPLEMENTATION))->willReturn('FakeTestModelClass');
+
+        $this->testsService->getTestModelImplementation($testModelProphecy->reveal());
+
+    }
+
+    public function testGetTestModelImplementationBackwardCompatible(){
+        $testModelProphecy = $this->prophesize('\core_kernel_classes_Resource');
+        $testModelProphecy->getOnePropertyValue(new core_kernel_classes_Property(PROPERTY_TESTMODEL_IMPLEMENTATION))->willReturn('oat\\taoTests\\test\\TestModelUnit');
+
+        $testModelImp = $this->testsService->getTestModelImplementation($testModelProphecy->reveal());
+
+        $this->assertInstanceOf('oat\\taoTests\\test\\TestModelUnit', $testModelImp);
+    }
+
+
+    /**
+     * @expectedException \common_exception_NoImplementation
+     * @expectedExceptionMessage No implementation found for testmodel testModelUri
+     */
+    public function testGetTestModelImplementationNullTestModelImplementation(){
+        $testModelProphecy = $this->prophesize('\core_kernel_classes_Resource');
+        $testModelProphecy->getOnePropertyValue(new core_kernel_classes_Property(PROPERTY_TESTMODEL_IMPLEMENTATION))->willReturn('');
+        $testModelProphecy->getUri()->willReturn('testModelUri');
+        $testModelProphecy->getUri()->should(new CallTimesPrediction(1));
+
+        $this->testsService->getTestModelImplementation($testModelProphecy->reveal());
+    }
+
+    public function testGetTestModelImplementationService(){
+        $serviceManagerProphecy = $this->prophesize('oat\\oatbox\\service\\ServiceManager');
+        $serviceManagerProphecy->get('testModelServiceId')->willReturn(new TestModelUnit());
+
+
+        $testServiceMock = $this->getMockBuilder('taoTests_models_classes_TestsService')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getServiceManager'))
+            ->getMock();
+
+        $testServiceMock->expects($this->once())
+            ->method('getServiceManager')
+            ->willReturn($serviceManagerProphecy->reveal());
+        $testModelProphecy = $this->prophesize('\core_kernel_classes_Resource');
+        $testModelProphecy->getOnePropertyValue(new core_kernel_classes_Property(PROPERTY_TESTMODEL_IMPLEMENTATION))->willReturn('testModelServiceId');
+
+
+        $testModelImp = $testServiceMock->getTestModelImplementation($testModelProphecy->reveal());
+
+        $this->assertInstanceOf('oat\\taoTests\\test\\TestModelUnit', $testModelImp);
+
     }
     
     
@@ -366,6 +426,66 @@ class TestsTestCase extends TaoPhpUnitTestRunner {
     public function testDeleteTest($tests) {
 		$testInstance = $this->testsService->createInstance($tests);
 		$this->assertTrue($this->testsService->deleteTest($testInstance));
+    }
+
+}
+
+
+class TestModelUnit implements \taoTests_models_classes_TestModel{
+    /**
+     * @inheritDoc
+     */
+    public function prepareContent(core_kernel_classes_Resource $test, $items = array())
+    {
+        // TODO: Implement prepareContent() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteContent(core_kernel_classes_Resource $test)
+    {
+        // TODO: Implement deleteContent() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getItems(core_kernel_classes_Resource $test)
+    {
+        // TODO: Implement getItems() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAuthoringUrl(core_kernel_classes_Resource $test)
+    {
+        // TODO: Implement getAuthoringUrl() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function cloneContent(core_kernel_classes_Resource $source, core_kernel_classes_Resource $destination)
+    {
+        // TODO: Implement cloneContent() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCompilerClass()
+    {
+        // TODO: Implement getCompilerClass() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPackerClass()
+    {
+        // TODO: Implement getPackerClass() method.
     }
 
 }
