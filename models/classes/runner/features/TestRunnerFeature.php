173c9903db9caa0d77637d35bb6676e85fbebe08
Christophe Noël <christophe@taotesting.com>
2016-10-18 11:49:06 +0200

    added missing licence headers and comments

--- models/classes/runner/features/TestRunnerFeature.php
+++ models/classes/runner/features/TestRunnerFeature.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *
+ */
 
 namespace oat\taoTests\models\runner\features;
 
@@ -7,7 +25,7 @@ use oat\taoTests\models\runner\plugins\TestPlugin;
 
 /**
  * A test runner feature is a user feature that can be expressed by one or more test runner plugins.
- * They can be toggled at the delivery level.
+ * They can be toggled at the delivery level and work only in the new test runner.
  *
  * @author Christophe Noël <christophe@taotesting.com>
  */
