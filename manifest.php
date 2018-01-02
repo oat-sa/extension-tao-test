9e6c131d42649fb046f9c9a344634f6f6c184ad9
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2018-01-02 18:05:26 +0100

    update dependency to taobackoffice

--- manifest.php
+++ manifest.php
@@ -38,7 +38,7 @@ return array(
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'taoItems' => '>=2.20.1',
-        'taoBackOffice' => '>=0.8',
+        'taoBackOffice' => '>=1.3.0',
         'tao' => '>=15.0.0'
     ),
     'models' => array(
