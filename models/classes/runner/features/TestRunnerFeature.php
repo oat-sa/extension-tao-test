8771a62c38d0ed9017fc17236dc0bb6c31c6e116
Aleh Hutnikau <hutnikau@1pt.com>
2018-03-29 12:50:24 +0300

    change TestRunnerFeature properties visibility

--- models/classes/runner/features/TestRunnerFeature.php
+++ models/classes/runner/features/TestRunnerFeature.php
@@ -39,17 +39,17 @@ abstract class TestRunnerFeature implements PhpSerializable, LoggerAwareInterfac
     /**
      * @var string
      */
-    public $id;
+    protected $id;
 
     /**
      * @var string[] must match active test runner plugins Ids
      */
-    public $pluginsIds;
+    protected $pluginsIds;
 
     /**
      * @var bool Determine if the feature will be automatically enabled upon delivery creation
      */
-    public $isEnabledByDefault;
+    protected $isEnabledByDefault;
 
     /**
      * @var TestPlugin[] Used to check the existence of plugins Ids
