e400868ee8c605a03310c0b161e4d00a7a44d186
Ferenc Kiss <ferenc@taotesting.com>
2019-08-08 10:50:46 +0200

    fix versions

--- manifest.php
+++ manifest.php
@@ -31,13 +31,13 @@ return array(
     'label' => 'Test core extension',
     'description' => 'TAO Tests extension contains the abstraction of the test-runners, but requires an implementation in order to be able to run tests',
     'license' => 'GPL-2.0',
-    'version' => '13.1.1',
+    'version' => '13.2.0',
     'author' => 'Open Assessment Technologies, CRP Henri Tudor',
     'requires' => array(
         'generis' => '>=7.1.0',
         'taoItems' => '>=6.0.0',
         'taoBackOffice' => '>=3.0.0',
-        'tao' => '>=36.1.0'
+        'tao' => '>=38.5.0'
     ),
     'models' => array(
         'http://www.tao.lu/Ontologies/TAOTest.rdf',
