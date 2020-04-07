f18630372eaf450b13e65b43aaa28d0408478382
Augustas Nedzinskas <augustas@taotesting.com>
2020-04-07 09:54:47 +0200

    Timer adjustment interfaces

---
+++ models/classes/runner/time/TimerAdjustmentMapInterface.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ */
+
+namespace oat\taoTests\models\runner\time;
+
+
+/**
+ * Interface TimerAdjustmentMapInterface
+ *
+ * Describes an API for timer adjustment storage and access.
+ *
+ * @package oat\taoTests\models\classes\runner\time
+ */
+interface TimerAdjustmentMapInterface
+{
+    public const ACTION_INCREASE = 'increase';
+    public const ACTION_DECREASE = 'decrease';
+
+    /**
+     * Puts an entry to the map
+     * @param string $sourceId
+     * @param string $action
+     * @param int $seconds
+     * @return TimerAdjustmentMapInterface
+     */
+    public function put($sourceId, $action, $seconds);
+
+    /**
+     * Gets the calculated adjustment in seconds
+     * @param string $sourceId
+     * @return int
+     */
+    public function get($sourceId);
+
+    /**
+     * Removes an entry specified by $sourceId
+     * @param string $sourceId
+     * @return TimerAdjustmentMapInterface
+     */
+    public function remove($sourceId);
+
+    /**
+     * Clears the map of all entries
+     * @return TimerAdjustmentMapInterface
+     */
+    public function clear();
+}
---
+++ models/classes/runner/time/TimerAdjustmentServiceInterface.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ */
+
+namespace oat\taoTests\models\runner\time;
+
+
+use oat\taoDelivery\model\execution\DeliveryExecutionInterface;
+use qtism\data\QtiIdentifiable;
+
+/**
+ * Interface TimerAdjustmentInterface
+ *
+ * @package oat\taoTests\models\runner\time
+ */
+interface TimerAdjustmentServiceInterface
+{
+    public const SERVICE_ID = 'taoTests/TimerAdjustment';
+
+    /**
+     * Increases allotted time by supplied amount of seconds
+     * @param DeliveryExecutionInterface $deliveryExecution
+     * @param int $seconds
+     * @param QtiIdentifiable $source
+     * @return bool
+     */
+    public function increase($deliveryExecution, $seconds, $source = null);
+
+    /**
+     * Decreases allotted time by supplied amount of seconds
+     * @param DeliveryExecutionInterface $deliveryExecution
+     * @param int $seconds
+     * @param QtiIdentifiable $source
+     * @return bool
+     */
+    public function decrease($deliveryExecution, $seconds, $source = null);
+}
