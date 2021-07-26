71e087cad938ff865933fcb9b3ae9a540a787198
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2021-07-26 14:11:47 +0200

    chore: Fix in id path

--- views/cypress/utils/selectors.js
+++ views/cypress/utils/selectors.js
@@ -1,7 +1,7 @@
 export default {
     deleteTest: '[data-context="resource"][data-action="removeNode"]',
     deleteClass: '[data-context="resource"][data-action="removeNode"]',
-    moveClass: '[id="item-move-to"][data-context="resource"][data-action="moveTo"]',
+    moveClass: '[id="test-move-to"][data-context="resource"][data-action="moveTo"]',
     moveConfirmSelector: 'button[data-control="ok"]',
     addTest: '[data-context="resource"][data-action="instanciate"]',
     testForm: 'form[action="/taoTests/Tests/editTest"]',
