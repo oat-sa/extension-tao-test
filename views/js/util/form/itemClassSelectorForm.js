3feabf814e8e3d47b1080dffa4d72ca1bc1db345
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2024-05-21 17:59:28 +0200

    fix: Simplify form generation

--- views/js/util/form/itemClassSelectorForm.js
+++ views/js/util/form/itemClassSelectorForm.js
@@ -13,32 +13,16 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2019 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2024 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
- */
-/**
- * @author Martin Nicholson <martin@taotesting.com>
  */
 define([
     'jquery',
     'i18n',
     'ui/filter',
-    'ui/feedback',
-    'layout/actions',
-    'ui/taskQueue/taskQueue',
-    'ui/taskQueueButton/standardButton'
-], function ($, __, filterFactory, feedback, actionManager, taskQueue, taskCreationButtonFactory) {
+    'ui/feedback'
+], function ($, __, filterFactory, feedback) {
     'use strict';
-
-    /**
-     * wrapped the old jstree API used to refresh the tree and optionally select a resource
-     * @param {String} [uriResource] - the uri resource node to be selected
-     */
-    const refreshTree = function refreshTree(uriResource){
-        actionManager.trigger('refresh', {
-            uri : uriResource
-        });
-    };
 
     return {
         /**
@@ -55,10 +39,9 @@ define([
         createSelectorInput({
                                 $filterContainer,
                                 $inputElement,
-                                taskButton,
                                 dataProvider,
-                                inputPlaceholder = __('Select the test you want to publish to the test-takers'),
-                                inputLabel = __('Select the test')
+                                inputPlaceholder = __('Select the item destination class'),
+                                inputLabel = __('Select Item Destination')
                             }) {
             return filterFactory($filterContainer, {
                 placeholder: inputPlaceholder,
@@ -68,11 +51,6 @@ define([
             })
                 .on('change', function(selection) {
                     $inputElement.val(selection);
-                    if (selection) {
-                        taskButton.enable();
-                    } else {
-                        taskButton.disable();
-                    }
                 })
                 .on('request', function(params) {
                     dataProvider
@@ -89,89 +67,18 @@ define([
         },
 
         /**
-         * Replaces rendered submit input with a button that sends a task to taskQueue over AJAX
-         * @param {Object} options
-         * @param {jQuery} options.$form
-         * @param {jQuery} options.$reportContainer
-         * @param {Object} options.buttonTitle
-         * @param {Object} options.buttonLabel
-         * @returns {taskQueueButton}
-         */
-        replaceSubmitWithTaskButton({
-                                        $form,
-                                        $reportContainer,
-                                        buttonTitle = __('Publish the test'),
-                                        buttonLabel = __('Publish')
-                                    }) {
-            //find the old submitter
-            const $oldSubmitter = $form.find('.form-submitter');
-            //prepare the new component
-            const taskCreationButton = taskCreationButtonFactory({
-                type : 'info',
-                icon : 'delivery',
-                title : buttonTitle,
-                label : buttonLabel,
-                taskQueue : taskQueue,
-                taskCreationUrl : $form.prop('action'),
-                taskCreationData : function getTaskCreationData(){
-                    return $form.serializeArray();
-                },
-                taskReportContainer : $reportContainer
-            })
-                .on('finished', function(result){
-                    if (result
-                        && result.task
-                        && result.task.report
-                        && Array.isArray(result.task.report.children)
-                        && result.task.report.children.length
-                        && result.task.report.children[0]) {
-                        if(result.task.report.children[0].data
-                            && result.task.report.children[0].data.uriResource){
-                            feedback().info(__('%s completed', result.task.taskLabel), { encodeHtml: false });
-                            refreshTree(result.task.report.children[0].data.uriResource);
-                        }else{
-                            this.displayReport(result.task.report.children[0], __('Error'));
-                        }
-                    }
-                })
-                .on('continue', function(){
-                    refreshTree();
-                })
-                .on('error', function(err){
-                    //format and display error message to user
-                    feedback().error(err);
-                    this.trigger('finished');
-                })
-                .render($oldSubmitter.closest('.form-toolbar'))
-                .disable();
-
-            //replace the old submitter with the new one
-            $oldSubmitter.replaceWith(taskCreationButton.getElement());
-
-            return taskCreationButton;
-        },
-
-        /**
          * Set up the wizard form for publishing a TAO Local delivery
          * @param {jQuery} $form
          * @param {Object} providers - contains function(s) for fetching data
          */
         setupTaoLocalForm($form, providers) {
-            const $reportContainer = $form.closest('.content-block');
             const $filterContainer = $('.item-select-container', $form);
             const $inputElement = $('#item', $form);
-
-            // Replace submit button with taskQueue requester
-            const taskButton = this.replaceSubmitWithTaskButton({
-                $form,
-                $reportContainer
-            });
 
             // Enhanced selector input for tests:
             this.createSelectorInput({
                 $filterContainer,
                 $inputElement,
-                taskButton,
                 dataProvider: {
                     list: providers.listTests
                 }
