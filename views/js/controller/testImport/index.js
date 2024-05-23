9d0983739e7bd3ccc0fd19caa734584bb7fbb61b
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2024-05-23 08:05:40 +0200

    fix: fix licence

--- views/js/controller/testImport/index.js
+++ views/js/controller/testImport/index.js
@@ -13,11 +13,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2019 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2024 (original work) Open Assessment Technologies SA;
  *
- */
-/**
- * @author Martin Nicholson <martin@taotesting.com>
  */
 define([
     'jquery',
