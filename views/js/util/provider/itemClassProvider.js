f8eb7034b78be310dd27a9a290a80089117446ad
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2024-05-30 13:58:24 +0200

    fix: licence change, refactor code

--- views/js/util/provider/itemClassProvider.js
+++ views/js/util/provider/itemClassProvider.js
@@ -13,17 +13,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2019 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
+ * Copyright (c) 2024 (original work) Open Assessment Technologies SA;
  */
-/**
- * @author Martin Nicholson <martin@taotesting.com>
- */
-define([
-    'jquery',
-    'i18n',
-    'util/url'
-], function ($, __, urlUtils) {
+define(['jquery', 'i18n', 'util/url'], function ($, __, urlUtils) {
     'use strict';
 
     var providers = {
@@ -36,13 +28,9 @@ define([
             return new Promise(function (resolve, reject) {
                 $.ajax({
                     //Find and setup class list for items
-                    url: urlUtils.route('getItemClasses', 'SaSItems', 'taoItems'),
-                    data: {
-                        q: data.q,
-                        page: data.page
-                    },
-                    type: 'GET',
-                    dataType: 'JSON'
+                    url: urlUtils.route('getItemClasses', 'SaSItems', 'taoItems'), data: {
+                        q: data.q, page: data.page
+                    }, type: 'GET', dataType: 'JSON'
                 }).done(function (tests) {
                     if (tests) {
                         resolve(tests);
