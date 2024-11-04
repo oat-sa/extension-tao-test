86a3ae17941dd639f5fc8cab5a4e735f137d4ae5
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2024-11-04 16:30:52 +0100

    fix: bypass metadata import issue

--- models/classes/Translation/Form/Modifier/TranslationFormModifier.php
+++ models/classes/Translation/Form/Modifier/TranslationFormModifier.php
@@ -22,6 +22,7 @@ declare(strict_types=1);
 
 namespace oat\taoTests\models\Translation\Form\Modifier;
 
+use core_kernel_classes_Literal;
 use oat\generis\model\data\Ontology;
 use oat\tao\model\featureFlag\FeatureFlagCheckerInterface;
 use oat\tao\model\form\Modifier\AbstractFormModifier;
@@ -54,10 +55,13 @@ class TranslationFormModifier extends AbstractFormModifier
         $translationType = $instance->getOnePropertyValue(
             $this->ontology->getProperty(TaoOntology::PROPERTY_TRANSLATION_TYPE)
         );
+        $translationTypeUri = $translationType instanceof core_kernel_classes_Literal ?
+            (string)$translationType :
+            $translationType->getUri();
 
         if (
             empty($translationType)
-            || $translationType->getUri() === TaoOntology::PROPERTY_VALUE_TRANSLATION_TYPE_ORIGINAL
+            || $translationTypeUri === TaoOntology::PROPERTY_VALUE_TRANSLATION_TYPE_ORIGINAL
         ) {
             $form->removeElement(tao_helpers_Uri::encode(TaoTestOntology::PROPERTY_TRANSLATION_COMPLETION));
         }
